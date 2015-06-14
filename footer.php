7328a52e8dc9567135bb287dc69d8cb174098fa3
Byron Lutz <byronlutz@gmail.com>
2015-06-13 18:30:08 -0700

    Initial commit

---
+++ footer.php
@@ -0,0 +1,18 @@
+	</div> <!-- End mainbody -->
+	
+<footer>
+<?php if(is_home()): ?>
+<p>Sigrist Homes is an "in-park" dealer offering new Redman and Skyline manufactured homes.  We are directly associated with the <a href="http://rocklinestates.com">Rocklin Estates Mobile Home Park in Rocklin, CA</a>, a senior community, and the <a href="http://sutterstreetmhp.com">Sutter Street Mobile Home Park in Yuba City, CA</a>.</p>
+<?php endif; ?>
+
+<p class="contactfooter">
+(916)390-5561 | <a href="mailto:info@sigristhomes.com">info@sigristhomes.com</a> | 5505 S. Grove St., Rocklin, CA
+</p>
+<p>
+<a href="<?php echo get_permalink(294); ?>">Privacy Policy</a> | &copy; Sigrist Homes <?php echo date('Y'); ?>
+</p>
+<?php wp_footer(); ?>
+<script src="<?php echo bloginfo('template_directory'); ?>/js/jquery.placeholder.js"></script>
+</body>
+
+</html>
